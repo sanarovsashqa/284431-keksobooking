25cfa1beda438e21638c68638ce6a59813cf088a
sanarovsashqa <alex.sanarov@gmail.com>
2018-06-09 22:28:00 +0300

    Рефакторинг кода, исправляет ошибки

--- js/map.js
+++ js/map.js
@@ -1,5 +1,34 @@
 'use strict';
+/**
+ * @typedef {Objeсt} Price
+ * @property {number} MIN
+ * @property {number} MAX
+ */
 
+/**
+ * @typedef {Objeсt} Room
+ * @property {number} MIN
+ * @property {number} MAX
+ */
+
+/**
+ * @typedef {Objeсt} Guest
+ * @property {number} MIN
+ * @property {number} MAX
+ */
+
+/**
+ * @typedef {Objeсt} OfferParams
+ * @property {Array.<string>} TITLES
+ * @property {Array.<string>} TYPES
+ * @property {Array.<string>} TIMES
+ * @property {Array.<string>} FEAUTURES
+ * @property {Array.<string>} PHOTOS
+ * @property {Array.<string>} TYPES
+ * @property {Price}
+ * @property {Room}
+ * @property {Guest}
+ */
 var offerParams = {
   TITLES: [
     'Большая уютная квартира',
@@ -49,6 +78,33 @@ var offerParams = {
   }
 };
 
+/**
+ * @typedef {Objeсt} AvatarParams
+ * @property {number} MIN_AVATARS
+ * @property {number} MAX_AVATARS
+ */
+var avatarParams = {
+  MIN_AVATARS: 8,
+  MAX_AVATARS: 99
+};
+
+/**
+ * @typedef {Objeсt} LocationX
+ * @property {number} MIN
+ * @property {number} MAX
+ */
+
+/**
+ * @typedef {Objeсt} LocationY
+ * @property {number} MIN
+ * @property {number} MAX
+ */
+
+/**
+ * @typedef {Objeсt} LocationParams
+ * @property {LocationX}
+ * @property {LocationY}
+ */
 var locationParams = {
   x: {
     MIN: 300,
@@ -60,16 +116,40 @@ var locationParams = {
   }
 };
 
+/**
+ * @typedef {Objeсt} PinParams
+ * @property {number} WIDTH
+ * @property {number} HEIGHT
+ */
 var pinParams = {
-  WIDTH: 40,
-  HEIGHT: 40
+  WIDTH: 50,
+  HEIGHT: 70
 };
 
+/**
+ * @typedef {Objeсt} X
+ * @property {string} palace
+ * @property {string} flat
+ * @property {string} bungalo
+ * @property {string} house
+ */
 var offerTypesTranslation = {
   palace: 'Дворец',
   flat: 'Квартира',
   bungalo: 'Бунгало',
   house: 'Дом'
+};
+
+/**
+ * @typedef {Objeсt} NewPhotoParams
+ * @property {number} WIDTH
+ * @property {number} HEIGHT
+ * @property {string} ALT
+ */
+var newPhotoParams = {
+  WIDTH: 40,
+  HEIGHT: 40,
+  ALT: 'Фотография жилья'
 };
 
 var AD_NUM = 8;
@@ -103,9 +183,13 @@ var getRandomInt = function (min, max) {
 };
 
 /**
+ * @typedef {Objeсt} CloneObject
+ */
+
+/**
  * Функция клонирования объекта
  * @param {Object} original - клонируемый объект
- * @return {Object}
+ * @return {CloneObject}
  */
 var getCopy = function (original) {
   var clone = Object.create(Object.getPrototypeOf(original));
@@ -121,11 +205,12 @@ var getCopy = function (original) {
 /**
  * Функция получения src для изображений
  * @param {number} num - Количество изображений
- * @return {Array}
+ * @param {number} max - Максимальное количество изображений
+ * @return {Array.<string>}
  */
-var getAvatarList = function (num) {
+var getAvatarList = function (num, max) {
   var avatarsList = [];
-  for (var i = 1; i <= num; i++) {
+  for (var i = 1; i <= num && i <= max; i++) {
     var avatar = 'img/avatars/user0' + i + '.png';
     avatarsList.push(avatar);
   }
@@ -134,52 +219,92 @@ var getAvatarList = function (num) {
 };
 
 /**
- * Функция получения информации для объявления
- * @param {number} num - количество объявлений
- * @return {Array}
+ * @typedef {Object} Author
+ * @property {string} avatar
  */
-var getAdData = function (num) {
+
+/**
+ * @typedef {Object} Offer
+ * @property {string} title
+ * @property {string} adress
+ * @property {number} price
+ * @property {string} type
+ * @property {number} rooms
+ * @property {number} guests
+ * @property {string} checkin
+ * @property {string} checkout
+ * @property {Array.<string>} features
+ * @property {string} description
+ * @property {Array.<string>} photos
+ */
+
+/**
+ * @typedef {Object} Location
+ * @property {number} x
+ * @property {number} y
+ */
+
+/**
+ * @typedef {Object} Ad
+ * @property {Author}
+ * @property {Offer}
+ * @property {Location}
+ */
+
+/**
+ * Функция получения информации для объявления
+ * @param {number} index
+ * @return {Ad}
+ */
+var getAdData = function (index) {
+
+  var avatars = shuffleArray(getAvatarList(avatarParams.MIN_AVATARS, avatarParams.MAX_AVATARS));
+  var titles = shuffleArray(offerParams.TITLES);
+  var locationX = getRandomInt(locationParams.x.MIN, locationParams.x.MAX);
+  var locationY = getRandomInt(locationParams.y.MIN, locationParams.y.MAX);
+  var price = getRandomInt(offerParams.price.MIN, offerParams.price.MAX);
+  var type = offerParams.TYPES[getRandomInt(0, offerParams.TYPES.length)];
+  var room = getRandomInt(offerParams.room.MIN, offerParams.room.MAX);
+  var guest = getRandomInt(offerParams.guest.MIN, offerParams.guest.MAX);
+  var checkin = offerParams.TIMES[getRandomInt(0, offerParams.TIMES.length)];
+  var checkout = offerParams.TIMES[getRandomInt(0, offerParams.TIMES.length)];
+  var feautures = shuffleArray(getCopy(offerParams.FEAUTURES)).splice(getRandomInt(0, offerParams.FEAUTURES.length));
+  var photos = shuffleArray(getCopy(offerParams.PHOTOS));
+
+  return {
+    author: {
+      avatar: avatars[index]
+    },
+    offer: {
+      title: titles[index],
+      adress: locationX + ', ' + locationY,
+      price: price,
+      type: type,
+      rooms: room,
+      guests: guest,
+      checkin: checkin,
+      checkout: checkout,
+      feautures: feautures,
+      description: '',
+      photos: photos
+    },
+    location: {
+      x: locationX,
+      y: locationY
+    }
+  };
+};
+
+/**
+ * Функция генерации объявлений
+ * @param {number} num - Количсетво объявлений
+ * @return {Array.<Ad>}
+ */
+var createAdData = function (num) {
   var adsData = [];
 
-  var avatars = shuffleArray(getAvatarList(num));
-  var titles = shuffleArray(offerParams.TITLES);
-
   for (var i = 0; i < num; i++) {
-    var locationX = getRandomInt(locationParams.x.MIN, locationParams.x.MAX);
-    var locationY = getRandomInt(locationParams.y.MIN, locationParams.y.MAX);
-    var price = getRandomInt(offerParams.price.MIN, offerParams.price.MAX);
-    var type = offerParams.TYPES[getRandomInt(0, offerParams.TYPES.length)];
-    var room = getRandomInt(offerParams.room.MIN, offerParams.room.MAX);
-    var guest = getRandomInt(offerParams.guest.MIN, offerParams.guest.MAX);
-    var checkin = offerParams.TIMES[getRandomInt(0, offerParams.TIMES.length)];
-    var checkout = offerParams.TIMES[getRandomInt(0, offerParams.TIMES.length)];
-    var feautures = shuffleArray(getCopy(offerParams.FEAUTURES)).splice(getRandomInt(0, offerParams.FEAUTURES.length));
-    var photos = shuffleArray(getCopy(offerParams.PHOTOS));
-
-    var ad = {
-      author: {
-        avatar: avatars[i]
-      },
-      offer: {
-        title: titles[i],
-        adress: locationX + ', ' + locationY,
-        price: price,
-        type: type,
-        rooms: room,
-        guests: guest,
-        checkin: checkin,
-        checkout: checkout,
-        feautures: feautures,
-        description: '',
-        photos: photos
-      },
-      location: {
-        x: locationX - pinParams.WIDTH / 2,
-        y: locationY - pinParams.HEIGHT
-      }
-    };
-
-    adsData.push(ad);
+    adsData.push(getAdData(i));
   }
 
   return adsData;
@@ -187,32 +312,32 @@ var getAdData = function (num) {
 
 /**
  * Функция создания метки на карте
- * @param {Array} arr
+ * @param {Array.<Object>} adData
  * @param {number} index
  * @return {Node}
  */
-var createPin = function (arr, index) {
+var createPin = function (adData) {
   var mapPinElement = mapPinTemplate.cloneNode(true);
   var mapPinImg = mapPinTemplate.querySelector('img');
 
-  mapPinElement.style.left = arr[index].location.x + 'px';
-  mapPinElement.style.top = arr[index].location.y + 'px';
-  mapPinImg.src = arr[index].author.avatar;
-  mapPinImg.alt = arr[index].offer.title;
+  mapPinElement.style.left = adData.location.x - pinParams.WIDTH / 2 + 'px';
+  mapPinElement.style.top = adData.location.y - pinParams.HEIGHT + 'px';
+  mapPinImg.src = adData.author.avatar;
+  mapPinImg.alt = adData.offer.title;
 
   return mapPinElement;
 };
 
 /**
  * Функция отрисовки меток
- * @param {Array} arr
+ * @param {Array.<Object>} adData
  * @return {DocumentFragment}
  */
-var renderPin = function (arr) {
+var renderPin = function (adData) {
   var fragment = document.createDocumentFragment();
 
-  for (var i = 0; i < arr.length; i++) {
-    fragment.appendChild(createPin(arr, i));
+  for (var i = 0; i < adData.length; i++) {
+    fragment.appendChild(createPin(adData[i]));
   }
 
   return fragment;
@@ -240,9 +365,9 @@ var createPhotoElement = function (element) {
   var newPhoto = document.createElement('img');
 
   newPhoto.classList.add('popup__photo');
-  newPhoto.width = 40;
-  newPhoto.height = 40;
-  newPhoto.alt = 'Фотография жилья';
+  newPhoto.width = newPhotoParams.WIDTH;
+  newPhoto.height = newPhotoParams.HEIGHT;
+  newPhoto.alt = newPhotoParams.ALT;
   newPhoto.src = element;
 
   return newPhoto;
@@ -250,29 +375,29 @@ var createPhotoElement = function (element) {
 
 /**
  * Функция создания карточки объявления
- * @param {Array} arr
+ * @param {Array.<Object>} adData
  * @param {number} index
  * @return {Node}
  */
-var createMapCard = function (arr, index) {
+var createMapCard = function (adData) {
   var mapCardElement = mapCardTemplate.cloneNode(true);
 
-  mapCardElement.querySelector('.popup__title').textContent = arr[index].offer.title;
-  mapCardElement.querySelector('.popup__text--address').textContent = arr[index].offer.adress;
-  mapCardElement.querySelector('.popup__text--price').textContent = arr[index].offer.price + '₽/ночь';
-  mapCardElement.querySelector('.popup__type').textContent = offerTypesTranslation[arr[index].offer.type];
-  mapCardElement.querySelector('.popup__text--capacity').textContent = arr[index].offer.rooms + ' комнаты для ' + arr[index].offer.guests + ' гостей';
-  mapCardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + arr[index].offer.checkin + ', выезд до ' + arr[index].offer.checkout;
-  mapCardElement.querySelector('.popup__description').textContent = arr[index].offer.description;
-  mapCardElement.querySelector('.popup__avatar').src = arr[index].author.avatar;
+  mapCardElement.querySelector('.popup__title').textContent = adData.offer.title;
+  mapCardElement.querySelector('.popup__text--address').textContent = adData.offer.adress;
+  mapCardElement.querySelector('.popup__text--price').textContent = adData.offer.price + '₽/ночь';
+  mapCardElement.querySelector('.popup__type').textContent = offerTypesTranslation[adData.offer.type];
+  mapCardElement.querySelector('.popup__text--capacity').textContent = adData.offer.rooms + ' комнаты для ' + adData.offer.guests + ' гостей';
+  mapCardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + adData.offer.checkin + ', выезд до ' + adData.offer.checkout;
+  mapCardElement.querySelector('.popup__description').textContent = adData.offer.description;
+  mapCardElement.querySelector('.popup__avatar').src = adData.author.avatar;
 
   var feautureFragment = document.createDocumentFragment();
-  arr[index].offer.feautures.forEach(function (element) {
+  adData.offer.feautures.forEach(function (element) {
     feautureFragment.appendChild(createFeautureElement(element));
   });
 
   var photoFragment = document.createDocumentFragment();
-  arr[index].offer.photos.forEach(function (element) {
+  adData.offer.photos.forEach(function (element) {
     photoFragment.appendChild(createPhotoElement(element));
   });
 
@@ -284,15 +409,13 @@ var createMapCard = function (arr, index) {
 
 /**
  * Функция отрисовки карточек объявления
- * @param {Array} arr
+ * @param {Array.<Object>} adData
  * @return {DocumentFragment}
  */
-var renderMapCard = function (arr) {
+var renderMapCard = function (adData) {
   var fragment = document.createDocumentFragment();
 
-  for (var i = 0; i < arr.length; i++) {
-    fragment.appendChild(createMapCard(arr, i));
-  }
+  fragment.appendChild(createMapCard(adData[0]));
 
   return fragment;
 };
@@ -304,7 +427,7 @@ var initPage = function () {
   var map = document.querySelector('.map');
   var mapPin = map.querySelector('.map__pins');
 
-  var adsListData = getAdData(AD_NUM);
+  var adsListData = createAdData(AD_NUM);
   var mapPinRender = renderPin(adsListData);
   var mapCardRender = renderMapCard(adsListData);
 
